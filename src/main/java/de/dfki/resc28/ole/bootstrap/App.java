84aaaacfaa667c1e070fc0d7525da4b004089f7f
René Schubotz <rene.schubotz@dfki.de>
2016-09-01 13:17:16 +0200

    Fuseki dump contianing all official ldraw parts.

--- src/main/java/de/dfki/resc28/ole/bootstrap/App.java
+++ src/main/java/de/dfki/resc28/ole/bootstrap/App.java
@@ -9,6 +9,7 @@ package de.dfki.resc28.ole.bootstrap;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +18,7 @@ import java.util.Properties;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.apache.commons.io.FilenameUtils;
@@ -46,6 +48,8 @@ public class App
 	public static String fBaseURI = null;
 	public static String fPartsDirectory = null;
 	public static IGraphStore fGraphStore = null;
+	public static Model fRepoModel = null;
+	public static Resource fRepo = null;
 
 
 	public static synchronized void configure()
@@ -96,9 +100,9 @@ public class App
 	public static void main( String[] args ) throws IOException
 	{
 		configure();
-		
+		initRepoModel();
 
-//		String partsDirectory = "/Users/resc01/Desktop/ldraw/ldraw/parts";
+//		parseFile(new File("/Users/resc01/Desktop/ldraw/ldraw/parts/995.dat"));		
 		
 		File[] files = new File(fPartsDirectory).listFiles(new FilenameFilter() {
 		    public boolean accept(File dir, String name) {
@@ -106,83 +110,79 @@ public class App
 		    }
 		});
 		
-		
-		Model repoModel = ModelFactory.createDefaultModel();
-		repoModel.setNsPrefixes(FOAF.NAMESPACE);
-		repoModel.setNsPrefixes(ADMS.NAMESPACE);
-		repoModel.setNsPrefixes(DCAT.NAMESPACE);
-		repoModel.setNsPrefix("dcterms", DCTerms.NS);
-		repoModel.setNsPrefix("rdf", RDF.getURI());
-		repoModel.setNsPrefix("rdfs", RDFS.getURI());
-		repoModel.setNsPrefix("skos", SKOS.getURI());
-		repoModel.setNsPrefix("xsd", XSD.NS);
-		repoModel.setNsPrefix("ldraw", "http://www.ldraw.org/ns/ldraw#");
-		repoModel.setNsPrefix("repo", fBaseURI + "repo/");
-		repoModel.setNsPrefix("users", fBaseURI + "repo/users/");
-		repoModel.setNsPrefix("assets", fBaseURI + "repo/assets/");
-		repoModel.setNsPrefix("distributions", fBaseURI + "repo/distributions/");
-		
-		Resource repo = repoModel.createResource(fBaseURI + "repo");
-		repoModel.add( repo, RDF.type, ADMS.AssetRepository );
-		repoModel.add( repo, DCTerms.title, "The Open Lego Parts Repository" );
-		repoModel.add( repo, DCTerms.created, repoModel.createTypedLiteral(Calendar.getInstance().getTime(),XSDDatatype.XSDdate));
-		repoModel.add( repo, DCTerms.description, "A complete catalog of LDraw parts" );
-		repoModel.add( repo, DCTerms.publisher, "resc28" );
-		
-		
-
-		int fileCounter = 0;
-		
 		for (File file : files) 
-		{
+			parseFile(file);
+		
+		fGraphStore.addToNamedGraph(fRepo.getURI(), fRepoModel);
 
-			if (file.isFile()) 
-			{	
-				
-				System.out.println(file.getAbsolutePath());
-				
-				// parse the .DAT file and create RDF models for asset and its .DAT distribution
-				InputStream fis = new FileInputStream(file);
-				LDrawLexer lexer = new LDrawLexer(new ANTLRInputStream(fis));
-				LDrawParser parser = new LDrawParser(new CommonTokenStream(lexer));
-				ParseTreeWalker walker = new ParseTreeWalker();
-				ParseTree tree = parser.file();	
-				
-				// create asset
-				walker.walk(new AssetListener(file.getName(), fGraphStore), tree);		
-				
-//				// create users
-//				walker.walk(new UserListener(file.getName(), fGraphStore), tree);
-				
-				
-				// create create ldraw-distribution
-				walker.walk(new LdrawDistributionListener(file.getName(), fGraphStore), tree);
-				
-				
-				// create ARVIDA distribution
-				
-				
-				// add asset to repo
-				Resource asset = repoModel.createResource(fBaseURI + "repo/assets/" + FilenameUtils.getBaseName(file.getName()));
-				repoModel.add( repo, DCAT.dataset, asset );
-			
-				// close InputStream
-				fis.close();			
-				fileCounter++;
-				
-				if (fileCounter == 100)
-					break;
-			}
-		}
-		
-		// put repo model to graphStore
-//		fGraphStore.addToDefaultGraph(repoModel);
-		fGraphStore.addToNamedGraph(repo.getURI(), repoModel);
-		
-		
-		System.out.println("Parsed assets: " + fileCounter);
 		System.exit(0);
 	}
 	
 	
+	
+	
+	private static void initRepoModel()
+	{
+		fRepoModel = ModelFactory.createDefaultModel();
+		fRepoModel.setNsPrefixes(FOAF.NAMESPACE);
+		fRepoModel.setNsPrefixes(ADMS.NAMESPACE);
+		fRepoModel.setNsPrefixes(DCAT.NAMESPACE);
+		fRepoModel.setNsPrefix("dcterms", DCTerms.NS);
+		fRepoModel.setNsPrefix("rdf", RDF.getURI());
+		fRepoModel.setNsPrefix("rdfs", RDFS.getURI());
+		fRepoModel.setNsPrefix("skos", SKOS.getURI());
+		fRepoModel.setNsPrefix("xsd", XSD.NS);
+		fRepoModel.setNsPrefix("ldraw", "http://www.ldraw.org/ns/ldraw#");
+		fRepoModel.setNsPrefix("repo", fBaseURI + "repo/");
+		fRepoModel.setNsPrefix("users", fBaseURI + "repo/users/");
+		fRepoModel.setNsPrefix("assets", fBaseURI + "repo/assets/");
+		fRepoModel.setNsPrefix("distributions", fBaseURI + "repo/distributions/");
+		
+		fRepo = fRepoModel.createResource(fBaseURI + "repo");
+		fRepoModel.add( fRepo, RDF.type, ADMS.AssetRepository );
+		fRepoModel.add( fRepo, DCTerms.title, "The Open Lego Parts Repository" );
+		fRepoModel.add( fRepo, DCTerms.created, fRepoModel.createTypedLiteral(Calendar.getInstance().getTime(),XSDDatatype.XSDdate));
+		fRepoModel.add( fRepo, DCTerms.description, "A complete catalog of LDraw parts" );
+		fRepoModel.add( fRepo, DCTerms.publisher, "resc28" );
+		
+		fGraphStore.createNamedGraph(fRepo.getURI(), fRepoModel);
+	}
+	
+	
+	private static void parseFile(File file) throws IOException
+	{
+		if (file.isFile()) 
+		{	
+			
+			System.out.println(file.getAbsolutePath());
+			
+			// parse the .DAT file and create RDF models for asset and its .DAT distribution
+			InputStream fis = new FileInputStream(file);
+			LDrawLexer lexer = new LDrawLexer(new ANTLRInputStream(fis));
+			LDrawParser parser = new LDrawParser(new CommonTokenStream(lexer));
+			ParseTreeWalker walker = new ParseTreeWalker();
+			ParseTree tree = parser.file();	
+			
+			// create asset
+			walker.walk(new AssetListener(file.getName(), fGraphStore), tree);		
+			
+//			// create users
+//			walker.walk(new UserListener(file.getName(), fGraphStore), tree);
+			
+			
+			// create create ldraw-distribution
+			walker.walk(new LdrawDistributionListener(file.getName(), fGraphStore), tree);
+			
+			
+			// create ARVIDA distribution
+			
+			
+			// add asset to repo
+			Resource asset = fRepoModel.createResource(fBaseURI + "repo/assets/" + FilenameUtils.getBaseName(file.getName()));
+			fRepoModel.add( fRepo, DCAT.dataset, asset );
+		
+			// close InputStream
+			fis.close();			
+		}
+	}
 }
